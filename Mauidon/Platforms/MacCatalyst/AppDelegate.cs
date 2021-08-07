48359807be108e93a1a3dadcb61eb0f91f375bd3
Tim Miller <innerlogic4321@gmail.com>
2021-08-06 22:52:29 -0400

    Add initial iOS Oauth Implementation

--- Mauidon/Platforms/MacCatalyst/AppDelegate.cs
+++ Mauidon/Platforms/MacCatalyst/AppDelegate.cs
@@ -2,8 +2,12 @@
 // Copyright (c) Drastic Actions. All rights reserved.
 // </copyright>
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Foundation;
 using Microsoft.Maui;
+using UIKit;
 
 namespace Mauidon
 {
@@ -13,5 +17,18 @@ namespace Mauidon
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate<Startup>
     {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+             global::Xamarin.Auth.Presenters.XamarinIOS.AuthenticationConfiguration.Init();
+             return base.FinishedLaunching(app, options);
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            var mapp = MauiUIApplicationDelegate.Current.Application as App;
+            var test = url.AbsoluteUrl.ToString().Split("code=").Last();
+            mapp.ShowAuthorizationModalAsync(test);
+            return true;
+        }
     }
 }
--- Mauidon/Platforms/iOS/AppDelegate.cs
+++ Mauidon/Platforms/iOS/AppDelegate.cs
@@ -1,5 +1,9 @@
-﻿using Foundation;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
 using Microsoft.Maui;
+using UIKit;
 
 namespace Mauidon
 {
@@ -9,5 +13,18 @@ namespace Mauidon
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate<Startup>
     {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+             global::Xamarin.Auth.Presenters.XamarinIOS.AuthenticationConfiguration.Init();
+             return base.FinishedLaunching(app, options);
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            var mapp = MauiUIApplicationDelegate.Current.Application as App;
+            var test = url.AbsoluteUrl.ToString().Split("code=").Last();
+            mapp.ShowAuthorizationModalAsync(test);
+            return true;
+        }
     }
 }
