c174514a17a5954c9034a4d5e469c499529fe67c
Tim Miller <innerlogic4321@gmail.com>
2021-09-01 17:22:01 -0400

    Update Templates

---
+++ Mauidon/Views/TootView.xaml.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="TootView.xaml.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mastonet.Entities;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace Mauidon.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TootView : ContentView
+    {
+        public TootView()
+        {
+            InitializeComponent();
+        }
+
+        public Status Status
+        {
+            get => (Status)this.GetValue(StatusProperty);
+            set => this.SetValue(StatusProperty, value);
+        }
+
+        public static readonly BindableProperty StatusProperty = BindableProperty.Create(
+                nameof(Status),
+                typeof(Status),
+                typeof(TootView),
+                null,
+                propertyChanging: (bindable, oldValue, newValue) =>
+                {
+                    var control = bindable as TootView;
+                    control.Status = newValue as Status;
+                });
+    }
+}
